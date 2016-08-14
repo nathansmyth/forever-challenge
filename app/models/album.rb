e593340948f65a89868ad0cea900d1fd775b6671
Nathan Smyth <email@nathansmyth.com>
2016-08-14 14:39:40 -0400

    Add Album model validations for position and average_date
    Add tests to cover Album validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,15 @@
 class Album < ActiveRecord::Base
   has_many :photos
+
   validates :name, presence: true
+  validates :position, numericality: { only_integer: true }
+
+  validate :valid_date
+
+  private
+  def valid_date
+    if((average_date.is_a?(Date) rescue ArgumentError) == ArgumentError)
+      errors.add(:average_date, 'Invalid Date Entered.')
+    end
+  end
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -5,15 +5,39 @@ require 'rails_helper'
 # each album's average_date field is average of all photos' taken_at values
 
 RSpec.describe Album, type: :model do
-  it 'has a name' do
-    expect(Album.name).to be
+  context 'with valid attributes' do
+    it 'has a name' do
+      expect(Album.name).to be
+    end
+
+    it 'has optional numeric position' do
+      expect(build(:album, name: 'test name', position: 5)).to be_valid
+      expect(build(:album, name: 'test name')).to be_valid
+    end
+
+    it 'has optional date average_date' do
+      expect(build(:album, name: 'test name', average_date: Date.new)).to be_valid
+      expect(build(:album, name: 'test name', average_date: nil)).to be_valid
+    end
+
+    it 'has a valid factory' do
+      expect(build(:album)).to be_valid
+    end
   end
 
-  it 'has a valid factory' do
-    expect(build(:album)).to be_valid
-  end
+  context 'with invalid attributes' do
+    it 'is invalid without a name' do
+      expect(build(:album, name: nil)).to_not be_valid
+    end
 
-  it 'is invalid without a name' do
-    expect(build(:album, name: nil)).to_not be_valid
+    it 'is invalid with non-integer position' do
+      expect(build(:album, name: 'test name', position: 'test position')).to_not be_valid
+      expect(build(:album, name: 'test name', position: nil)).to_not be_valid
+      expect(build(:album, name: 'test name', position: Date.new)).to_not be_valid
+    end
+
+    it 'is valid with non-date average_date' do
+      expect(build(:album, name: 'test name', average_date: 'test average_date').average_date).to be_nil
+    end
   end
 end
