0f0bfdc19ec21434e6ad110a47794cb8c58734a2
Nathan Smyth <email@nathansmyth.com>
2016-08-14 14:50:07 -0400

    Add Photo create method with photo_params filtering method

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,16 @@
 class PhotosController < ApplicationController
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      render json: @photo, status: :created
+    else
+      render json: @photo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:name, :position, :average_date)
+  end
 end
