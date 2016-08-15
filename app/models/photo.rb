0680b8da9d1995768c47901c54117eb4da399393
Nathan Smyth <email@nathansmyth.com>
2016-08-15 19:23:16 -0400

    Update Photo model to require url

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,12 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
   validates :name, presence: true
+  validates :url, presence: true
   validate :valid_url?
 
   private
   def valid_url?
-    unless url.end_with?('jpg', 'jpeg') || url.blank?
+    unless url.end_with?('jpg', 'jpeg')
       errors.add(:url, 'Invalid URL Entered.')
     end
   end
