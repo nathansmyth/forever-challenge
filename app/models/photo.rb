6db16fec5e744c14a53349f46cd0699dc0eb299a
Nathan Smyth <email@nathansmyth.com>
2016-08-15 19:14:13 -0400

    Add photo validation for jpg/jpeg extension

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,12 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
   validates :name, presence: true
+  validate :valid_url?
+
+  private
+  def valid_url?
+    unless url.end_with?('jpg', 'jpeg') || url.blank?
+      errors.add(:url, 'Invalid URL Entered.')
+    end
+  end
 end
