fea31e4e04ec7127d35404e057b2c9bb3ce8e4d8
Nathan Smyth <email@nathansmyth.com>
2016-08-17 21:23:34 -0400

    Add pagination specs

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -16,8 +16,31 @@ RSpec.describe AlbumsController, type: :controller do
         expect(response).to have_http_status(200)
 
         data = JSON.parse(response.body, object_class: OpenStruct)
-        expect(data.albums.count).to eq(15)
+        expect(data.albums.count).to eq(10)
+      end
+
+      it 'includes photo count with each album' do
+        populate_albums
+        get :index
+
+        data = JSON.parse(response.body, object_class: OpenStruct)
         expect(data.albums[0].photo_count).to be >= 0
+      end
+
+      it 'paginates the albums, 10 at a time' do
+        populate_albums
+        get :index
+
+        data = JSON.parse(response.body, object_class: OpenStruct)
+        expect(Album.page.length).to eq(10)
+      end
+
+      it 'paginates the albums, with page index param' do
+        populate_albums
+        get :index, {:page => 2}
+
+        data = JSON.parse(response.body, object_class: OpenStruct)
+        expect(Album.page(2).length).to eq(5)
       end
 
       it 'shows the album' do
@@ -28,15 +51,15 @@ RSpec.describe AlbumsController, type: :controller do
         expect(Album.count).to eq(1)
       end
 
+      it 'responds with 201' do
+        post :create, album: attributes_for(:album), format: :json
+        expect(response).to have_http_status(201)
+      end
+
       it 'updates the album' do
       end
 
       it 'destroys the album' do
-      end
-
-      it 'responds with 201' do
-        post :create, album: attributes_for(:album), format: :json
-        expect(response).to have_http_status(201)
       end
     end
 
